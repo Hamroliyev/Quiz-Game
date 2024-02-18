59d2fca368cf9f7083d545e3a43f44c8f668f3e7
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-02-18 12:37:15 +0500

    Build Basis Of Code

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("\t... Welcome Quiz Game ... ");
+int quizNumber = 1;
+int correctAnswers = 0;
+Random random = new Random();
+char[] alphabet = Enumerable.Range('A', 26).Select(asciiCode => (char)asciiCode).ToArray();
+
+do
+{
+
+    quizNumber++;
+} while (quizNumber < 4);
